9b133891db81cea44811ab71c297c800a3c4ab9b
johannesambrosch <johannes.ambrosch@gmail.com>
2018-07-22 01:03:27 +0200

    Add Sample scene

---
+++ Unity/EnterTheRoom/Assets/Scripts/Legacy/ChangingSprite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingSprite : MonoBehaviour {
+
+    public Sprite regular, alternative;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    internal void switchSprite(bool alt)
+    {
+        if (alt)
+        {
+            GetComponent<SpriteRenderer>().sprite = alternative;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = regular;
+        }
+    }
+}
---
+++ Unity/EnterTheRoom/Assets/VRTK/Examples/ExampleResources/Scripts/Whirlygig.cs
@@ -0,0 +1,39 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Whirlygig : VRTK_InteractableObject
+    {
+
+        bool alt = true;
+        public ChangingSprite cs;
+
+        float spinSpeed = 0f;
+        Transform rotator;
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+            spinSpeed = 360f;
+            cs.switchSprite(alt);
+            alt = !alt;
+        }
+
+        public override void StopUsing(VRTK_InteractUse usingObject)
+        {
+            base.StopUsing(usingObject);
+            spinSpeed = 0f;
+        }
+
+        protected void Start()
+        {
+            rotator = transform.Find("Capsule");
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+        }
+    }
+}
